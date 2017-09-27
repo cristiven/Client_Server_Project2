8b7ce3f31bd049e4bd0e8be8e80018c149575776
cristiven <cristiven@utp.edu.co>
2017-09-27 18:37:34 -0500

    Modelos del chat

--- concafer/sgc/models.py
+++ concafer/sgc/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -32,3 +33,14 @@ class SuperNumerario(models.Model):
     nombre = models.CharField(max_length=20)
     contrasena = models.CharField(max_length=20)
 
+# Modelos para el chat
+
+class Room(models.Model):
+    name = models.TextField()
+    label = models.SlugField(unique=True)
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages')
+    handle = models.TextField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now, db_index=True)
--- concafer/sgc/views.py
+++ concafer/sgc/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from .forms import LogForm 
 from .models import Recolector
+from .models import Room
 
 #@login_required
 def home(request):
@@ -35,3 +36,18 @@ def login(request):
 	}
 	return render(request, 'login_b.html', context)
 	
+
+# vistas para el chat
+
+def chat_room(request, label):
+    # If the room with the given label doesn't exist, automatically create it
+    # upon first visit (a la etherpad).
+    room, created = Room.objects.get_or_create(label=label)
+
+    # We want to show the last 50 messages, ordered most-recent-last
+    messages = reversed(room.messages.order_by('-timestamp')[:50])
+
+    return render(request, "chat/room.html", {
+        'room': room,
+        'messages': messages,
+    })
