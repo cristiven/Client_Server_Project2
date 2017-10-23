6a49887d0c5f97510d591518e78ab8b591132f9c
cristiven <cristiven@utp.edu.co>
2017-10-23 02:11:31 -0500

    Mod Settings

--- concafer/concafer/settings.py
+++ concafer/concafer/settings.py
@@ -20,7 +20,7 @@ from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
